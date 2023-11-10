be918fa57294d18fbeb2596ae18e1b3c18450068
Anupam Singh <79111711+AnupamAS01@users.noreply.github.com>
2023-11-10 08:13:00 +0530

    Initial commit

---
+++ Example/NielsenAppSDK/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  NielsenAppSDK
+//
+//  Created by AnupamAS0x1 on 11/10/2023.
+//  Copyright (c) 2023 AnupamAS0x1. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+  func applicationDidFinishLaunching(_ aNotification: Notification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(_ aNotification: Notification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
---
+++ Example/NielsenAppSDK/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  NielsenAppSDK
+//
+//  Created by AnupamAS0x1 on 11/10/2023.
+//  Copyright (c) 2023 AnupamAS0x1. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: Any? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
